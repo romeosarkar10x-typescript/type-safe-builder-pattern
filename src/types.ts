16911b0c9957f3c3ba4be2af68d1c06ffe8e2235
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-01-01 15:46:27 +0530

    Removed comment

--- src/types.ts
+++ src/types.ts
@@ -43,15 +43,6 @@ export type BuilderAddProperty<
 > = string extends Key
     ? never
     : PrettifyBuilderObject<BuilderDoAddProperty<Key, Value, BuilderObject>>;
-/*
-export type BuilderAddProperty<
-    Key extends string,
-    Value,
-    BuilderObject extends object = object,
-> = string extends Key
-    ? never
-    : BuilderDoAddProperty<Key, Value, BuilderObject>;
-    */
 
 export type PrettifyBuilderObject<T extends Record<string, unknown>> = {
     [K in keyof T]: T[K];
